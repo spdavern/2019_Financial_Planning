780bf208e2b8cb4a3d3a2ba24014fe18b176fba0
spdavern <sean@lizandsean.com>
2019-06-05 10:07:58 -0700

    Started simplifying Dev Script into project code snipet that does the extract, transform, and load steps.

---
+++ code/ETL.R
@@ -0,0 +1,15 @@
+# Extract the giving data from the Excel file:
+library("readxl")
+giving.data <- read_excel("./data/Giving Data.xlsx")
+
+# Extract a copy of selected columns of the original data to manipulate.
+df <- select(
+  .data = giving.data,
+  week.ending = 'Week Ending',
+  month = Month,
+  year = Year,
+  paypal = Paypal,
+  offering = Offering,
+  total = Total,
+  monthly.giving.families = 'Monthly # families'
+)
